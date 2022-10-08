d2f1a2e30ed33da2c17ab091a9afbf49d882bd4a
gg <gg@gmail.com>
2022-10-08 04:08:41 +0300

    [reworked]"compile.bat"

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-g++ -o main -I D:\six_hot_loads\SFML-2.5.1\include\SFML -L D:\six_hot_loads\SFML-2.5.1\lib\SFML main.cpp
+g++ -o main -I D:\six_hot_loads\SFML-2.5.1\include -L D:\six_hot_loads\SFML-2.5.1\lib main.cpp
